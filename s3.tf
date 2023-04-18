d8d13c8780c4fa61b3beafc42d1c5b2a1be9b85e
god0y <84692497+god0y@users.noreply.github.com>
2023-04-18 00:44:05 -0300

    add

--- s3.tf
+++ s3.tf
@@ -27,7 +27,7 @@ resource "aws_s3_bucket_policy" "this" {
 resource "aws_s3_bucket_lifecycle_configuration" "bucket-config" {
   bucket = aws_s3_bucket.this.id
   rule {
-    id     = "archival"
+    id     = "archivalsssss"
     status = "Enabled"
     transition {
       days          = 365
